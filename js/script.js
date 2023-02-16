b65ef6527bb3801680f4d0ce4fd8d5666b90cc00
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-16 10:46:54 +0000

    Added click event to id of random at script.js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    // Get Information when id of random is clicked
+    $("#random").click(function() {
+        // Set Up Variable
+        let txt = "";
+        // Get the List Items
+        const listItems = $("li");
+        // Get the Number of List Items
+        const listItemsLength = listItems.length;
+        // Create a new Array
+        const listItemsArray = Array(listItemsLength).fill("").map((_,i) => listItems[i].innerHTML);
+        // Get the Random Index
+        const randomIndex = Math.floor(Math.random() * listItemsLength);
+        // Get the Random Celebrity
+        const celebrity = listItems[randomIndex].innerHTML;
+        // Build the Information
+        txt += `There are ${listItemsLength} celebrities.<p>`;
+        txt += `The celebrities are ${listItemsArray}.<p>`;
+        txt += `${celebrity} is selected at random.`;
+        // Display Information in the Browser
+        $("#display").html(txt);
+        // Change the Colour of the List Item
+        $("li:eq(" + randomIndex + ")").css("color","#D2386C");
+    });
+});
